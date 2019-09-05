8e6f9f317ad20a5b4b4c6610a41f8366230b8de8
George Tsopanoglou <gtsop.priv@gmail.com>
2019-09-05 23:22:54 +0100

    Provide urls

--- dotfiles/bashrc/behaviours/editor.sh
+++ dotfiles/bashrc/behaviours/editor.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+# https://wiki.archlinux.org/index.php/Environment_variables#Default_programs
+# https://unix.stackexchange.com/a/73486
+#
 
 export VISUAL=vim
 export EDITOR="$VISUAL"
