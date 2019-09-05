4603cb7c0e99e9e1944cddccf0375606302f7986
George Tsopanoglou <gtsop.priv@gmail.com>
2019-09-05 23:04:58 +0100

    Make script runable from outside the directory

--- install.sh
+++ install.sh
@@ -3,6 +3,11 @@
 # install.sh
 #
 
+SCRIPT_PATH=`dirname $(readlink -f "${BASH_SOURCE[0]}")`
+CURRENT_PATH=$(pwd)
+
+cd $SCRIPT_PATH
+
 # Install packages
 sudo pacman -S --noconfirm - < packages.txt
 
@@ -12,3 +17,5 @@ ln -sf $(pwd)/dotfiles/bashrc/.bashrc ~/.bashrc
 # Install scripts to usr path
 sudo ln -sf $(pwd)/scripts/caps-to-esc.sh /usr/local/bin/caps2esc
 sudo ln -sf $(pwd)/scripts/todo.sh /usr/local/bin/todo
+
+cd $CURRENT_PATH
