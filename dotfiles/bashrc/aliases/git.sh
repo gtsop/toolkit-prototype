65138ee0b3a8b130cf53d7d1ee9d76d41e9c745d
George Tsopanoglou <gtsop.priv@gmail.com>
2019-09-07 23:05:31 +0100

    Add git config aliases

--- dotfiles/bashrc/aliases/git.sh
+++ dotfiles/bashrc/aliases/git.sh
@@ -3,6 +3,8 @@
 alias ga='git add --all'
 alias gb='git branch'
 alias gc='git commit -m'
+alias gcue='git config --global user.email'
+alias gcun='git config --global user.name'
 alias gch='git checkout'
 alias gd='git diff HEAD'
 alias gpl='git pull'
